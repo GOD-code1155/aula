89f7adc9808f0e4fef987736a1a0a7682173cd3a
francisco.davi <francisco.davi@aluno.edu.pi.gov.br>
2026-06-17 12:56:35 +0000

    atividade feita

---
+++ divisao.js
@@ -0,0 +1,22 @@
+const calculadora ={
+    numero1: 10,
+    numero2: 5,
+    multiplicar(){
+        return this.numero1 / this.numero2
+    },
+    const calculadora ={
+        numero1: 10,
+        numero2: 5,
+        const calculadora ={
+            numero1: 10,
+            numero2: 5,
+            multiplicar(){
+                return this.numero1 *this.numero2
+            },
+            divisao(){
+                return this.numero1 *this.numero2
+      
+    }
+    
+}
+        console.log(calculadora . multiplicar())
--- objguncao.js
+++ objguncao.js
@@ -3,6 +3,18 @@ const calculadora ={
     numero2: 5,
     multiplicar(){
         return this.numero1 *this.numero2
-    }
+    },
+    divisao(){
+        return this.numero1 / this.numero2
+    },
+    soma(){
+        return this.numero1 + this.numero2
+    },
+    subtracao(){
+        return this.numero1 - this.numero2
+    }   
 }
-        console.log(calculadora . multiplicar())
+        console.log(calculadora.multiplicar())
+        console.log(calculadora.divisao())
+        console.log(calculadora.soma())
+        console.log(calculadora.subtracao())
