a65960914d135fa835f14f46e78753a0d9346146
francisco.davi <francisco.davi@aluno.edu.pi.gov.br>
2026-06-25 17:12:19 +0000

    atividade feita

--- biblioteca.js
+++ biblioteca.js
@@ -15,11 +15,36 @@ const biblioteca = {
         this.livros.push(livro);
     },
 
-    mostrarLivros(){
-        for(let i=0; i<this.livros.length; i++){
-            const L= this.livros[i];
-            console.log(L.nome + " - " + L.autor + " - " + L.ano + " - " + L.genero);
+    mostrarLivros() {
+        for (let i = 0; i < this.livros.length; i++) {
+            const L = this.livros[i];
+            console.log(
+                `${L.nome} - ${L.autor} - ${L.ano} - ${L.genero}`
+            );
         }
+    },
+
+    alugar(livro) {
+        for (let i = 0; i < this.livros.length; i++) {
+            const L = this.livros[i];
+
+            if (L.nome === livro) {
+
+                if (!L.disponivel) {
+                    console.log("Livro indisponível!");
+                    return;
+                }
+
+                L.disponivel = false;
+                L.alugado = true;
+                L.vezesAlugado++;
+
+                console.log("Livro alugado com sucesso!");
+                return;
+            }
+        }
+
+        console.log("Livro não encontrado!");
     }
 };
 
@@ -37,5 +62,8 @@ biblioteca.adicionarLivros(
     "Fantasia"
 );
 
-console.log(biblioteca.livros);
 biblioteca.mostrarLivros();
+
+biblioteca.alugar("The Alchemist");
+
+console.log(biblioteca.livros);
